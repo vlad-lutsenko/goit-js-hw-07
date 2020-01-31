c08ccc40401fd364880bad2da8f801c8b2d458c3
Vlad Lutsenko <lutsenko.vladyslav@gmail.com>
2020-01-31 09:45:35 +0200

    task-5

--- task-5.js
+++ task-5.js
@@ -1,11 +1,4 @@
 "use strict";
-//Напиши скрипт который, при наборе текста в инпуте
-// input#name-input (событие input), подставляет его
-//текущее значение в span#name-output. Если инпут пустой,
-// в спане должна отображаться строка 'незнакомец'.
-
-//<input type="text" placeholder="Ваше имя?" id="name-input" />
-//<h1>Привет, <span id="name-output">незнакомец</span>!</h1>
 
 const input = document.querySelector("#name-input");
 
